32b06485aa235ea8eaebe1d98c84709f560f9d5e
febriatama009 <febriatama009@gmail.com>
2023-03-19 13:41:12 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,5 @@
+/* eslint-disable max-len */
+// Memuat data notes yang disimpan dalam bentuk array objek.
+const notes = [];
+
+module.exports = notes;
---
+++ src/routes.js
@@ -0,0 +1,41 @@
+/* eslint-disable max-len */
+
+const {
+  addNoteHandler, getAllNotesHandler, getAllNotesByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler,
+} = require('./handler');
+
+// Memuat kode konfigurasi routing server seperti menentukan path, method, dan handler yang digunakan
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getAllNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
